f15c78e75d60779d349d33ba8f11a84efb30ebc2
Katie Marriner <kemarriner@gmail.coml>
2025-04-21 16:56:39 -0400

    check dev

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import json from '@rollup/plugin-json';
 
+
+const dev = import.meta.env.DEV;
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: dev ? '' : '/the-state-of-housing',
   plugins: [
     svelte(),
     json({
