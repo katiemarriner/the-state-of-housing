b15016f9ecf506a5e4835ae8282598aa7dbeb6cb
Katie Marriner <kemarriner@gmail.coml>
2025-04-21 16:59:22 -0400

    fix url loc

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,12 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import json from '@rollup/plugin-json';
 
-
-const dev = import.meta.env.DEV;
-
 // https://vite.dev/config/
-export default defineConfig({
-  base: dev ? '' : '/the-state-of-housing',
-  plugins: [
-    svelte(),
-    json({
-      compact: true
-    })
-  ],
-})
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'; // 'serve' for dev, 'build' for prod
+
+  return {
+    base: isDev ? '/' : '/the-state-of-housing',
+    plugins: [svelte(), json()],
+  };
+});
