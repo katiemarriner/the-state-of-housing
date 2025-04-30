904a18bba4b289f8f05ce8262ee24d75e47a31f8
Katie Marriner <kemarriner@gmail.coml>
2025-04-30 14:52:09 -0400

    start to set up search

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,23 +1,82 @@
 <script>
-  import { onMount } from 'svelte';
+  import countiesData from  '../counties.store.js';
+  
+  let counties
+  countiesData.subscribe(items => counties = items);
+  
+  let inputValue;
+  let filteredCounties;
+  const filterCounties = () => {
+    filteredCounties = counties.filter(county => {
+      return county.county_name.startsWith(inputValue);
+    });
+  }
 
-  const url = import.meta.env.BASE_URL;
-  let fips;
-  onMount(async() => {
-    const res = await fetch(`${url}/data/fips.json`);
-    fips = await res.json();
-  })
-
-  $: console.log(fips)
+  $: console.log(filteredCounties);
 </script>
 
 <h1>The State of the American Housing Market</h1>
-<form>
-  <input type="search" placeholder="Search for your county will be here" />
+<form autocomplete="off">
+  <div class="autocomplete" style="width:300px;">
+    <input id="county-search-input" type="text" name="county-search" bind:value={inputValue} placeholder="Search for a county" on:input={filterCounties}>
+  </div>
+  <input type="submit">
 </form>
 
 <ul>
-  {#each fips as fip}
-    <li><a href="#/county/{fip}">{fip}</a></li>
+  {#each counties as county}
+    <a href="#/county/{county.county_fips}"><li>{county.county_name}</li></a>
   {/each}
 </ul>
+
+<style lang="scss">
+  * { box-sizing: border-box; }
+  body {
+    font: 16px Arial;
+  }
+  .autocomplete {
+    /*the container must be positioned relative:*/
+    position: relative;
+    display: inline-block;
+  }
+  input {
+    border: 1px solid transparent;
+    background-color: #f1f1f1;
+    padding: 10px;
+    font-size: 16px;
+  }
+  input[type=text] {
+    background-color: #f1f1f1;
+    width: 100%;
+  }
+  input[type=submit] {
+    background-color: DodgerBlue;
+    color: #fff;
+  }
+  .autocomplete-items {
+    position: absolute;
+    border: 1px solid #d4d4d4;
+    border-bottom: none;
+    border-top: none;
+    z-index: 99;
+    /*position the autocomplete items to be the same width as the container:*/
+    top: 100%;
+    left: 0;
+    right: 0;
+  }
+  .autocomplete-items div {
+    padding: 10px;
+    cursor: pointer;
+    background-color: #fff;
+    border-bottom: 1px solid #d4d4d4;
+  }
+  .autocomplete-items div:hover {
+    /*when hovering an item:*/
+    background-color: #e9e9e9;
+  }
+  .autocomplete-active {
+    /*when navigating through the items using the arrow keys:*/
+    background-color: DodgerBlue !important;
+    color: #ffffff;
+  }
+</style>
--- tasks/format-realtor-data-by-county.js
+++ tasks/format-realtor-data-by-county.js
@@ -1,43 +1,45 @@
 import io from 'indian-ocean';
 import _ from 'underscore';
 
-const data = io.readDataSync('RDC_Inventory_Core_Metrics_County_History.csv');
+const countyData = io.readDataSync('data/RDC_Inventory_Core_Metrics_County_History.csv');
+const nationalData = io.readDataSync('data/listing_weekly_core_aggregate_by_country.csv');
+const fips = io.readDataSync('data/county_fips.csv');
 
-let meta = _.unique(data, 'county_fips');
-meta = meta.map(d => {
+const meta = fips.map(d => {
   return {
-    county_fips: d.county_fips,
-    county_name: d.county_name
+    county_fips: d.FP,
+    county_name: d.NAME
   }
 });
 
-function formatDate(date_) {
-  return `${date_.substring(0, 4)}-${date_.substring(4, 6)}-01`;
-};
+// function formatDate(date_) {
+//   return `${date_.substring(0, 4)}-${date_.substring(4, 6)}-01`;
+// };
 
-const max = Number(data[0].month_date_yyyymm);
-const twoYears = max - 600;
+// const max = Number(data[0].month_date_yyyymm);
+// const twoYears = max - 600;
 
-const medianPriceTimeseries = data.filter(d => {
-    return Number(d.month_date_yyyymm) >= twoYears;
-  }).map(d => {
-    return {
-      county_fips: d.county_fips,
-      median_listing_price: d.median_listing_price,
-      month_date_yyyymm: formatDate(d.month_date_yyyymm)
-    }
-  });
+// const medianPriceTimeseries = countyData.filter(d => {
+//     return Number(d.month_date_yyyymm) >= twoYears;
+//   }).map(d => {
+//     return {
+//       county_fips: d.county_fips,
+//       median_listing_price: d.median_listing_price,
+//       median_listing_price: d.median_listing_price,
+//       month_date_yyyymm: formatDate(d.month_date_yyyymm)
+//     }
+//   });
 
-const combined = _.indexBy(meta.map(d => {
-  return {
-    county_fips: d.county_fips,
-    county_name: d.county_name, 
-    timeseries: medianPriceTimeseries.filter(month_ => month_.county_fips === d.county_fips)
-  }
-}), 'county_fips');
+// const combined = _.indexBy(meta.map(d => {
+//   return {
+//     county_fips: d.county_fips,
+//     county_name: d.county_name, 
+//     medianListingPrice: medianPriceTimeseries.filter(month_ => month_.county_fips === d.county_fips),
+//   }
+// }), 'county_fips');
 
-Object.keys(combined).forEach(key => {
-  io.writeDataSync(`../public/data/counties/${key}.json`, combined[key]);
-});
+// Object.keys(combined).forEach(key => {
+//   io.writeDataSync(`../public/data/counties/${key}.json`, combined[key]);
+// });
 
-io.writeDataSync('../public/data/fips.json', Object.keys(combined));
+io.writeDataSync('../public/data/fips.json', meta);
