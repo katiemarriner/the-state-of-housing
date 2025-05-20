fa86e6836e960ff1169aa9da3e39c2e250bb243e
Katie Marriner <kemarriner@gmail.coml>
2025-05-20 14:10:02 -0400

    add a github link

--- src/components/NationalBanner.svelte
+++ src/components/NationalBanner.svelte
@@ -15,7 +15,7 @@
 {#if national}
 <div class="container-banner-national">
   <marquee><h4><span class="purple">National metrics: Median listing price: { formats.currency(national.latest['median_listing_price']) }</span>  |  <span class="orange">Change in inventory from { getOneYearAgo(national.latest['month_date']) } is { formats.percent(national.latest['active_listing_count_yoy']) }</span>  |  The median amount of days on the market is { national.latest['median_days_on_market'] }</h4></marquee>
-  <div class="label">Last update: { time.monthYearFormat(time.parseTime(national['latest']['month_date'])) }</div>
+  <div class="label"><span class="label-update">Last update: { time.monthYearFormat(time.parseTime(national['latest']['month_date'])) }</span> | <a href="https://github.com/katiemarriner/the-state-of-housing">Contribute on GitHub</a></div>
 </div>
 {/if}
 
@@ -34,8 +34,14 @@
   }
 
   .label {
-    color: variables.$red;
     text-transform: uppercase;
-    font-weight: 700;
+
+    .label-update {
+      color: variables.$red;
+    }
+
+    a, span {
+      font-weight: 700;
+    }
   }
 </style>
