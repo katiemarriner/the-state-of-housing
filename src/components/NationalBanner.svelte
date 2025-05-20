e38ec19f344873119480f1ffb695339168391a4c
Katie Marriner <kemarriner@gmail.coml>
2025-05-20 13:27:04 -0400

    fix latest date label

--- src/components/NationalBanner.svelte
+++ src/components/NationalBanner.svelte
@@ -6,18 +6,16 @@
   const { formats, time } = helpers;
 
   $: national = $dataStoreCommon.historicalNational;
-  $: latestDate = $dataStoreCommon.latestDate;
 
   function getOneYearAgo(date_) {
     return time.monthYearFormat(timeYear.offset(time.parseTime(date_), -1));
   }
-
 </script>
 
 {#if national}
 <div class="container-banner-national">
   <marquee><h4><span class="purple">National metrics: Median listing price: { formats.currency(national.latest['median_listing_price']) }</span>  |  <span class="orange">Change in inventory from { getOneYearAgo(national.latest['month_date']) } is { formats.percent(national.latest['active_listing_count_yoy']) }</span>  |  The median amount of days on the market is { national.latest['median_days_on_market'] }</h4></marquee>
-  <div class="label">Last update: { time.monthYearFormat(time.parseTime(latestDate)) }</div>
+  <div class="label">Last update: { time.monthYearFormat(time.parseTime(national['latest']['month_date'])) }</div>
 </div>
 {/if}
 
